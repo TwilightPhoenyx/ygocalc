81175a15e0501c167af505dd34e3e4400f63541c
TwilightPhoenyx <gabrieldavidfs@hotmail.com>
2023-06-22 22:08:02 +0200

    Added Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { createContext, useState } from "react";
 import MaterialInput from "./components/MaterialInput";
 
-
+const MyContext = createContext();
 
 function App() {
   
-  const MyContext = createContext();
   const [statsByLvMaterial1, setStatsByLvMaterial1]= useState({})
   const [statsByLvMaterial2, setStatsByLvMaterial2]= useState({})
 
@@ -13,26 +12,26 @@ function App() {
 
 
   return (
-    <main>
-
       <MyContext.Provider value={ [fusionStats, setFusionStats] }>
-        <MaterialInput 
-          title="First Material"
-          materialName="material1" 
-          statsSetter={setStatsByLvMaterial1} 
-        />
-        <MaterialInput 
-          title="Second Material"
-          materialName="material2" 
-          statsSetter={setStatsByLvMaterial2} 
-        />
+        <main>
+          <MaterialInput 
+            title="First Material"
+            materialName="material1" 
+            statsSetter={setStatsByLvMaterial1} 
+          />
+          <MaterialInput 
+            title="Second Material"
+            materialName="material2" 
+            statsSetter={setStatsByLvMaterial2} 
+          />
+        </main>
       </MyContext.Provider>
       
-    </main>
+
   );
 }
 
 export default App;
 export {
   MyContext
-}
+};
